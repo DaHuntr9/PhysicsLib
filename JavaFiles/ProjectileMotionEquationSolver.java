8ae2411179a5911ec4f894773af0531f3b6c8cd6
Hunter <hunterdstudent@gmail.com>
2020-01-21 01:23:30 -0500

    added ProjectileMotionEquationSolver.java

---
+++ JavaFiles/ProjectileMotionEquationSolver.java
@@ -0,0 +1,8 @@
+package JavaFiles;
+
+/**	
+ * Projectile Motion Equation Solver to solve 2-D projectile motion equations.
+ */	
+public class ProjectileMotionEquationSolver implements CalculationTools{
+
+}
